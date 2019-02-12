79d386e203db95d5eb42c2f27b312a4432e5c1bc
wond-z <wond-z@live.com>
2019-02-12 10:26:59 +0800

    FrameTest AWT(abstract window toolkit)

---
+++ FrameTest/src/codes/FrameTest.java
@@ -0,0 +1,14 @@
+package codes;
+
+import java.awt.Frame;
+
+public class FrameTest {
+
+	public static void main(String[] args) {
+		Frame f = new Frame("测试窗口");
+		f.setBounds(30, 30, 300, 250);
+		f.setVisible(true);
+
+	}
+
+}
